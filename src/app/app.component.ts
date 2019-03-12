27f1ac7af546d7ebf6be15c8d65a98f1a13dc90a
FelipeCerero <lfcerero@misena.edu.co>
2019-03-11 20:26:24 -0500

    2019/03/11 20:25pm requerimientos de splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
   public appPages = [
     {
       title: 'Home',
@@ -26,6 +28,7 @@ export class AppComponent {
       icon: 'list'
     }
   ];
+  showSplash=true;
 
   constructor(
     private platform: Platform,
@@ -33,12 +36,15 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    this.splashScreen.show();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.splashScreen.show();
+
+      timer ( 3000).subscribe(()=>this.showSplash=false)
     });
   }
 }
